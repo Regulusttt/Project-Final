4d476e06fa1bc757e36d142b9362479c9894cf34
biancagsp <83022039+biancagsp@users.noreply.github.com>
2021-05-20 22:42:12 +0800

    Pembukaan program
    menu-menu program

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+ system("color 0c");
+    int pilih;
+
+//awal :
+    system("cls");
+    printf("=====================================================================================================================\n");
+    printf("\t\t\t\t  Selamat Datang di Resto Pokimane:V\n");
+    printf("\t\t\tContact Person : darryl,yanda,revi,bianca,teguh,ronaldi\n");
+    printf("=====================================================================================================================\n");
+    printf("\n1.Lihat Menu\n");
+    printf("2.Buat Order\n");
+    printf("3.Pulang\n\n");
+    printf("=====================================================================================================================\n");
+    printf("Masukan Pilihan : ");
+    scanf("%d",&pilih);
+    switch (pilih)
+    {
+    case 1:
+        {
+
+        }
+    case 2:
+        {
+            int buatorder;
+            puts("=====================================================================================================================");
+            puts("Buat Order :");
+            puts("1.Pesan Bundle");
+            puts("2.Custom Bundle");
+            puts("3.Pesan Perunit");
+            puts("4.Kembali ke Menu Utama");
+            puts("=====================================================================================================================");
+            printf("Masukkan Pilihan : ");
+            scanf("%d", &buatorder);
+            if(buatorder==1)
+            {
+
+            }
+            else if(buatorder==2)
+            {
+
+            }
+            else if(buatorder==3)
+            {
+
+            }
+            else if(buatorder==4)
+            {
+
+            }
+        }
+    case 3:
+        {
+
+        }
+}
